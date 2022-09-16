66889421c4448b2523a06d4fc5bbc6a5a86ed6b5
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-16 12:59:27 -0400

    added timer from class

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
  //////////////Varibles/////////////////////////////
 var generateBtn = document.querySelector("#startquiz");
 var test;
+var timer = document.getElementById("time-limit");
+
 
 
  //////////////Arrays?//////////////////////////////
 
  //////////////Funtions/////////////////////////////
+
+function timerfun(){
+   var startTime= 100;
+   var interval= setInterval(function(){
+      startTime--;
+      timer.textContent = " Time:  "  +startTime;
+      if (startTime===0){
+         // clearInterval(startTime);
+         
+      }
+   }, 1000)
+}
+
 
 function openBox(){
  if (confirm("its working") ){
@@ -14,7 +29,7 @@ function openBox(){
  } 
 }
 
-
+timerfun()
  //////////////Logic/////////////////////////////
 
 
