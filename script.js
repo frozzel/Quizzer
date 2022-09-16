f19008b5077fa8f0e6aef49ddd5368451821f9bc
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-09-16 12:00:35 -0400

    added some base to js learned how to call doc in class---next stepgit add -Agit add -A

---
+++ script.js
@@ -0,0 +1,22 @@
+ //////////////Varibles/////////////////////////////
+var generateBtn = document.querySelector("#startquiz");
+var test;
+
+
+ //////////////Arrays?//////////////////////////////
+
+ //////////////Funtions/////////////////////////////
+
+function openBox(){
+ if (confirm("its working") ){
+    var test = true
+    console.log(test)
+ } 
+}
+
+
+ //////////////Logic/////////////////////////////
+
+
+////Add some clics//////
+generateBtn.addEventListener("click", openBox);
